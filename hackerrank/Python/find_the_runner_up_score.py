dd100247cf6492f2a044b0e3c4fe0f1a6846da76
Islam Ayman <e-i.mohammed@lean.sa>
2021-05-19 06:10:21 +0200

    Hackerrank-Python: Runner up

---
+++ hackerrank/Python/find_the_runner_up_score.py
@@ -0,0 +1,10 @@
+def max(x, y):
+    print(x)
+    print(sorted(set(y))[-2])
+
+
+if __name__ == '__main__':
+    # n = int(input())
+    # arr = map(int, input().split())
+    max(4, [1, -1, -2, -1])
+    max(5, [2, 3, 6, 6, 5])
