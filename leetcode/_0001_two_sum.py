426b0628a3d1492c0ce73ff8b5bfff14ac0fb8de
Islam Ayman <e-i.mohammed@lean.sa>
2021-05-29 08:12:58 +0200

    Qualified: calculator

---
+++ leetcode/_0001_two_sum.py
@@ -0,0 +1,50 @@
+import timeit
+from typing import List
+
+
+class Solution:
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     i = 0
+    #     while i < len(nums) - 1:
+    #         for j in range(i + 1, len(nums)):
+    #             if nums[i] + nums[j] == target:
+    #                 return [i, j]
+    #         i += 1
+    #
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     for i, num in enumerate(nums):
+    #         try:
+    #             j = nums.index(target - num)
+    #             if -1 < j != i:
+    #                 return [i, j]
+    #         except ValueError:
+    #             pass
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     i = 0
+    #     j = len(nums) - 1
+    #     sortedNums = sorted(zip(nums, range(len(nums))))
+    #     while i < j:
+    #         if sortedNums[i][0] + sortedNums[j][0] == target:
+    #             return [sortedNums[i][1], sortedNums[j][1]]
+    #         elif sortedNums[i][0] + sortedNums[j][0] < target:
+    #             i += 1
+    #         elif sortedNums[i][0] + sortedNums[j][0] > target:
+    #             j -= 1
+    #
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        history = dict()
+
+        for i, n in enumerate(nums):
+            if target - n in history:
+                return [history[target-n], i]
+
+            history[n] = i
+
+
+if __name__ == '__main__':
+    nums = [3, 2, 4]
+    target = 6
+    print(Solution().twoSum(nums, target))
+    print(timeit.timeit(f"Solution().twoSum({nums}, {target})",
+                        number=10000,
+                        globals=globals()))
---
+++ qualified/practice_assessment.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = '12.3'
+    i = 0
+
+    val = 0
+    while i < len(a):
+        while a[i].isdigit() or a[i] == '.':
+            val += float()
+        i += 1
