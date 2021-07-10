d38a639ea3e8d45c1f732dc2ed32ced524d8fbc5
Islam Ayman <e-i.mohammed@lean.sa>
2021-07-10 13:42:59 +0400

    TESTDOME: python programming test

---
+++ testdome/programming_tests/python.py
@@ -0,0 +1,27 @@
+import timeit
+from math import sqrt
+
+class IceCreamMachine:
+    def __init__(self, ingredients, toppings):
+        self.ingredients = ingredients
+        self.toppings = toppings
+
+    def scoops(self):
+        return [[i, t] for i in self.ingredients for t in self.toppings]
+
+
+def find_roots(a, b, c):
+    d = sqrt(b ** 2 - 4 * a * c)
+    x1 = (-b + d) / (2 * a)
+    x2 = (-b - d) / (2 * a)
+
+    return x1, x2
+
+
+if __name__ == '__main__':
+    machine = IceCreamMachine(["vanilla", "chocolate"], ["chocolate sauce"])
+    print(
+        machine.scoops()
+    )  # should print[['vanilla', 'chocolate sauce'], ['chocolate', 'chocolate sauce']]
+
+    print(find_roots(2, 10, 8))
