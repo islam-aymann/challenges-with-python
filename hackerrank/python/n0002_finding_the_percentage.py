560bfcd54491f7e64e9ec905a1cb131deeab0087
Islam Ayman <e-i.mohammed@lean.sa>
2021-07-19 20:42:03 +0200

    HackerRank: Python: n0002_finding_the_percentage

---
+++ hackerrank/python/n0002_finding_the_percentage.py
@@ -0,0 +1,35 @@
+import timeit
+
+
+def find_the_percentage_of(n, student_marks, query_name):
+    student = student_marks[query_name]
+    print(f"{sum(student)/3:.2f}")
+
+
+if __name__ == "__main__":
+    # n = int(input())
+    # student_marks = {}
+    # for _ in range(n):
+    #     name, *line = input().split()
+    #     scores = list(map(float, line))
+    #     student_marks[name] = scores
+    # query_name = input()
+
+    n = 3
+    student_marks = {
+        "Krishna": [67, 68, 69],
+        "Arjun": [70, 98, 63],
+        "Malika": [52, 56, 60],
+    }
+    query_name = "Malika"
+
+    find_the_percentage_of(n, student_marks, query_name)
+    # print(
+    #     1000
+    #     * timeit.timeit(
+    #         lambda: find_the_percentage_of(n, student_marks, query_name),
+    #         number=10000,
+    #         globals=globals(),
+    #     ),
+    #     "ms",
+    # )
